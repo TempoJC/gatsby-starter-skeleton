cf7033a6582079218d787ae539e51f77c8d77892
Jose Luis Camargo Calizaya <joseluis.camargo@i3television.es>
2022-08-31 22:47:06 +0200

    feat: update package-lock

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,7 +4,7 @@ const config: GatsbyConfig = {
   siteMetadata: {
     title: `gatsby-starter-skeleton`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@tempojc88`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   graphqlTypegen: true,
